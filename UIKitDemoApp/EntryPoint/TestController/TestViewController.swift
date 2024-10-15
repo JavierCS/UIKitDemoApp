5adc8630ed3174733aff1d5e89cc91e2e4a0aafb
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2024-10-14 18:51:00 -0600

    [NEW] - Some testing code in TestViewController

--- UIKitDemoApp/EntryPoint/TestController/TestViewController.swift
+++ UIKitDemoApp/EntryPoint/TestController/TestViewController.swift
@@ -7,8 +7,22 @@
 
 import UIKit
 
+enum ViewVisivility {
+    case full
+    case intermediate
+    case hideen
+}
+
 class TestViewController: UIViewController {
     // MARK: - UIElements
+    @IBOutlet weak var someContentView: UIView!
+    @IBOutlet weak var topTableViewConstraint: NSLayoutConstraint!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Logic Vars
+    var viewVisivility: ViewVisivility = .full
+    var sizeToGrow: CGFloat = 150
+    var topTableConstraintConstant: CGFloat = 0
     
     // MARK: - Life Cycle Management
     override func viewDidLoad() {
@@ -19,5 +33,50 @@ class TestViewController: UIViewController {
     // MARK: - Configuration Management
     private func initialConfiguration() {
         title = "Test Controller"
+        configureTable()
+    }
+    
+    private func configureTable() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension TestViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "Celda \(indexPath.row)"
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
+extension TestViewController: UITableViewDelegate {
+    
+}
+
+extension TestViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.y
+        
+        if offset >= 0 && topTableConstraintConstant == -sizeToGrow {
+            return
+        }
+        
+        topTableConstraintConstant += -offset
+        if abs(topTableConstraintConstant) >= sizeToGrow {
+            topTableConstraintConstant = -sizeToGrow
+        } else if topTableConstraintConstant >= 0 {
+            topTableConstraintConstant = 0
+        }
+        
+        topTableViewConstraint.constant = topTableConstraintConstant
+        scrollView.contentOffset = .zero
     }
 }
